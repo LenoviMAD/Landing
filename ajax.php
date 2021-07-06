de75946fca46e3bc10ef08daf905cb43f320c350
LenoviMAD <madmoisess@gmail.com>
2021-07-06 11:32:25 -0400

    cambios landing

--- ajax.php
+++ ajax.php
@@ -5,45 +5,6 @@ $client = new xclient("");
 $util = new Util;
 session_start();
 
-//Se llama al servicio para llenar el select hijo
-if (isset($_GET["cond"])) {
-    if ($_GET["cond"] == "mgetproviderl") {
-        $data_json = $client->mgetproviderl($_GET["valor0"]);
-        print_r(json_encode($data_json));
-    }
-
-    if ($_GET["cond"] == "mgetcellphoneareacodel") {
-
-        $data_json = $client->mgetcellphoneareacodel($_GET["valor0"]);
-        print_r(json_encode($data_json));
-    }
-
-    if ($_GET["cond"] == "mgetcurrencysrcl") {
-        $data_json = $client->mgetcurrencysrcl($_GET["valor0"]);
-        print_r(json_encode($data_json));
-    }
-
-    if ($_GET["cond"] == "mgetinstrumentdstl") {
-        $data_json = $client->mgetinstrumentdstl($_GET["valor0"], $_GET["valor1"]);
-        print_r(json_encode($data_json));
-    }
-
-    if ($_GET["cond"] == "mgetcurrencydstl") {
-        $data_json = $client->mgetcurrencydstl($_GET["valor0"], $_GET["valor1"], $_GET["valor2"]);
-        print_r(json_encode($data_json));
-    }
-
-    if ($_GET["cond"] == "mgetremitancetypel") {
-        $data_json = $client->mgetremitancetypel($_GET["valor0"]);
-        print_r(json_encode($data_json));
-    }
-
-    if ($_GET["cond"] == "mcalcsend") {
-        $data_json = $client->mcalcsend(1, $_GET["valor0"], $_GET["valor1"], $_GET["valor2"]);
-        print_r(json_encode($data_json));
-    }
-}
-
 if (isset($_POST["cond"])) {
     if ($_POST["cond"] == "addlead") {
         $email = $_POST["email"];
